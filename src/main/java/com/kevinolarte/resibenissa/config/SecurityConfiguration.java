34fdb55c7cb66f7811603daddc84ee6a3bd293ef
kevin <kevinolarte.ko@gmail.com>
2025-04-08 11:45:02 +0200

    Desactivar la authenticacion de todos los EndPoint
    He desactivado la authenticacion porque aun no la tengo creada y queria
    porbar mi primmer controller de prueba que devuelve un helloWorld!º

---
+++ src/main/java/com/kevinolarte/resibenissa/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.kevinolarte.resibenissa.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
