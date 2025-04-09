60a5f967ad477f4ed2c4d5973f51895125748b89
kevin <kevinolarte.ko@gmail.com>
2025-04-09 14:10:06 +0200

    Reparar una ruta

--- src/main/java/com/kevinolarte/resibenissa/controllers/RegistroJuegoController.java
+++ src/main/java/com/kevinolarte/resibenissa/controllers/RegistroJuegoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/resi/juego/stats")
+@RequestMapping("/resi/juegos/stats")
 @RestController
 @AllArgsConstructor
 public class RegistroJuegoController {
